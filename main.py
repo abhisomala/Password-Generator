3959cd69ffec39292909b88f3be1dc40c00ffa42
asomala <somala.abhi@gmail.com>
2021-01-29 19:27:54 +0000

    Changed texts

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+#!/bin/python3
+
 import random
 
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+print('''
+Password Generator
+==================
+''')
 
-while 1:
-  password_len =  int(input("What length do you want your password to be?"))
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
+
+number = input('How many passwords do you want? ')
+number = int(number)
+
+length = input('What length do you want your password to be?')
+length = int(length)
+
+print('\nHere are your passwords:')
+
+for pwd in range(number):
+  password = ''
+  for c in range(length):
+    password += random.choice(chars)
+  print(password)
